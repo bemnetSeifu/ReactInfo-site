f56ef429ff72dcc4e1f1b6e8cb76b2ad8f483f2f
bemnetSeifu <bemnetseifu@gmail.com>
2024-08-25 01:49:26 +0300

    Initial  commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Body from "./components/body"
+import Navbar from "./components/Navbar"
+
+function App() {
+  
+  return (
+    <main className="container">
+      <Navbar />
+      <Body />
+    </main>
+  )
+}
+
+export default App
